89af0666e5c157c0dcb72e008ff27a2c2cb99ac7
GwonHyeok <me@ghyeok.io>
2018-01-22 01:37:11 +0900

    first commit

---
+++ template/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "{{ name }}",
+    application: {
+        components: 'dist/components.js',
+        services: 'dist/services/main.js',
+        plugins: 'dist/plugins.js'
+    }
+}
